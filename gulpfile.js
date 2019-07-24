fa243d22cd1f957d6c78f023928a362eb77877f8
James Atkinson <jamesatkinson117@gmail.com>
2019-07-24 16:11:19 +0100

    Gulp CSS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-function defaultTask(cb) {
-    cb();
+const { src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass');
+
+function css() {
+    return src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('css'))
 }
 
-exports.default = defaultTask
+exports.css = css;
+exports.default = parallel(css);

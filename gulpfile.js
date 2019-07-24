d8da8f724f6081810dd7a4353d77f449af9aa754
James Atkinson <jamesatkinson117@gmail.com>
2019-07-24 18:07:23 +0100

    Installed PostCSS and Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+const autoprefixer = require('autoprefixer');
 const { src, dest, watch } = require('gulp');
+const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 
 function css() {
     return src('sass/**/*.scss')
         .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
         .pipe(dest('css'))
 }
 
